5dc0001d373223bce15ffbbc5039711452b2b3cd
trPht <95902533+trPht@users.noreply.github.com>
2021-12-13 19:53:42 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,5 +1,5 @@
 const textConfig = {
-  text1: "He luu cậu!",
+  text1: "He luu Quỳnh!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
   text3: "Cậu yêu tớ có phải không nào ._.",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
@@ -7,7 +7,7 @@ const textConfig = {
   text6: "Yêu ơi là yêu <3",
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
+  text9: "Vì câu ngon vl<3<3",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
   text11:
     "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
